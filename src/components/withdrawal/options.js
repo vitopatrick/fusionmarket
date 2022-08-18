fc8a00d2e2556ba171b54d96231d71b186696bd6
Ikoro Godstime <fecypher@gmail.com>
2022-08-18 22:40:43 +0100

    changed UI

--- src/components/withdrawal/options.js
+++ src/components/withdrawal/options.js
@@ -1,22 +1,22 @@
 export const options = [
   {
     name: "Bitcoin",
-    minAmount: "$1000",
+    minAmount: "$50",
     duration: "48hrs",
   },
   {
     name: "Ethereum",
-    minAmount: "$1000",
+    minAmount: "$50",
     duration: "48hrs",
   },
   {
     name: "Litecoin",
-    minAmount: "$1000",
+    minAmount: "$50",
     duration: "48hrs",
   },
   {
     name: "Shiba",
-    minAmount: "$1000",
+    minAmount: "$50",
     duration: "48hrs",
   },
 ];
